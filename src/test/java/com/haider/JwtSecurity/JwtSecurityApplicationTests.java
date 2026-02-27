28d91f0699a6782b3c88d1df7f6c9258e9715b5b
sh8941 <zshamoonhaider@gmail.com>
2026-02-27 09:19:01 +0530

    Project structure created

---
+++ src/test/java/com/haider/JwtSecurity/JwtSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haider.JwtSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
